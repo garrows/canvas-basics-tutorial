35bde44a7b1e7cd3db1df3566306b4f97094f635
Glen Arrowsmith <glen.arrowsmith@gmail.com>
2015-09-05 22:19:29 +1000

    Bare minimum. Needs restructure

---
+++ gulpfile.js
@@ -0,0 +1,30 @@
+var gulp = require('gulp'),
+  livereload = require('gulp-livereload'),
+  nodemon = require('gulp-nodemon');
+
+gulp.task('run', function() {
+  livereload.listen();
+
+  nodemon({
+    script: 'bin/www',
+    ext: 'js',
+    ignore: ['public/*'],
+  }).on('restart', function() {
+    // when the app has restarted, run livereload.
+    setTimeout(function() {
+      gulp.src('bin/www')
+        .pipe(livereload())
+    }, 500);
+  })
+});
+
+gulp.task('reload', function() {
+  gulp.src('public/tutorial/*.html')
+    .pipe(livereload());
+});
+
+
+gulp.task('default', ['run'], function() {
+  livereload.listen();
+  gulp.watch('public/tutorial/*', ['reload']);
+});
