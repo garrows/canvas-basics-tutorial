4a5e818e7debe98411d87a8cd91bbf0852bce6da
Glen Arrowsmith <glen.arrowsmith@gmail.com>
2015-09-07 20:35:48 +1000

    Added source view

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'HTML5 Canvas 2D Basics Tutorial' });
 });
 
 module.exports = router;
